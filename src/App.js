c4cb76fc32e49ec8a4defdfc859c3ac81df885b7
IbrahimAlSuhaim <ka20@windowslive.com>
2019-09-14 03:05:56 +0300

    feat: add componentDidMount() to get all books from API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react'
 import './App.css'
-//import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import ListBooks from './ListBooks'
 
 class BooksApp extends Component {
   state = {
     books: []
   }
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState(() => ({
+          books
+        }))
+      })
+  }
+  updateBookShelf = (book, shelf) => {
+    book.shelf=shelf
+
+    BooksAPI.update(book, shelf)
+      .then((book) => {
+          this.setState((currentState) => ({
+            books: currentState.books.filter((b) => {
+                      return b.id !== book.id
+                    }).concat(book)
+          }))
+      })
+  }
+
   render() {
     return (
       <div className="app">
-        <ListBooks />
+        <ListBooks books={this.state.books} onUpdateBookShelf={this.updateBookShelf}/>
       </div>
     )
   }
