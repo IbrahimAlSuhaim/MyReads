a9b0520cbce98409a894cb158229204fff23fb69
IbrahimAlSuhaim <ka20@windowslive.com>
2019-09-14 03:08:16 +0300

    refactor: add shelf component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
-import CurrentlyReading from './CurrentlyReading'
-import WantToRead from './WantToRead'
-import Read from './Read'
+//import CurrentlyReading from './CurrentlyReading'
+//import WantToRead from './WantToRead'
+//import Read from './Read'
+import Shelf from './Shelf'
 
 class ListBooks extends Component {
+  booksOnShelf(shelf) {
+    const booksOnShelf = this.props.books.filter((book) => (
+      book.shelf===shelf
+    ))
+    return booksOnShelf
+  }
+
   render() {
+    const { onUpdateBookShelf } = this.props
     return (
       <div className="list-books">
         <div className="list-books-title">
@@ -12,9 +21,9 @@ class ListBooks extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            <CurrentlyReading />
-            <WantToRead />
-            <Read />
+            <Shelf shelf='Currently Reading' books={this.booksOnShelf('currentlyReading')} onUpdateBookShelf={(book, shelf) => {onUpdateBookShelf(book, shelf)}} />
+            <Shelf shelf='Want to Read' books={this.booksOnShelf('wantToRead')} onUpdateBookShelf={(book, shelf) => {onUpdateBookShelf(book, shelf)}} />
+            <Shelf shelf='Read' books={this.booksOnShelf('read')} onUpdateBookShelf={(book, shelf) => {onUpdateBookShelf(book, shelf)}} />
           </div>
         </div>
       </div>
