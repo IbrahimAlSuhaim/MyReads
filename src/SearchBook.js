3f6c9af985cddfe649c312b874294409459d0d97
IbrahimAlSuhaim <ka20@windowslive.com>
2019-09-16 21:28:55 +0300

    feat: add search feature

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import BookItem from './BookItem'
 
 class SearchBook extends Component {
+  state = {
+    query: '',
+    results: []
+  }
+  updateQuery = (query) => {
+    this.setState(() => ({
+      query: query.trim()
+
+    }))
+    query !== '' &&
+      // this.props.search(query)
+      BooksAPI.search(query)
+          .then((results) => {
+            this.setState(() => ({
+              results
+            }))
+          })
+  }
+  clearQuery = () => {
+    this.updateQuery('')
+  }
   render() {
+    const { query } = this.state
+    const { onUpdateBookShelf, inList } = this.props
+    const showingResult = query ==='' ?
+      []
+      : this.state.results
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+              />
 
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {
+              Array.isArray(showingResult) &&
+              showingResult.map((book) => (
+              <BookItem key={book.id} book={book} onUpdateBookShelf={onUpdateBookShelf} inList={inList} />
+            ))
+          }
+          </ol>
         </div>
       </div>
     )
